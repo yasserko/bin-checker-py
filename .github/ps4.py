cc8e047df090222c79c3cc60ed6546e446ca97a8
yasserko <78909015+yasserko@users.noreply.github.com>
2021-02-20 06:04:00 +0300

    Create ps4.py

---
+++ .github/ps4.py
@@ -0,0 +1,357 @@
+by Ahmedo Miziry Telegram: @AhmedoPlus
+x-x-x ACCOUNT: jimmyjonberry@hotmail.co.uk:Bubbles12
+---CheckTime: 2021/02/14 03:13 AM
+---ONLINE ID: JamesJonBerry
+---COUNTRY: GB
+---DATEOFBIRTH: 1994-08-18
+---DEACTIVATION: Yes
+---DEVICES: PS4[1] 
+---PLAYSTATION PLUS: PlayStation Plus: 12 Month Membership [12/30/2021]
+---PAYMENT METHODS: [VISA 2021-8] 
+---BALANCE: £0.00
+---HAS MORE TRANSACTIONS: NO
+-------------------- Transactions --------------------
+FUT 21 – FIFA Points 750 [4.99£][2021-02-10]
+FUT 21 – FIFA Points 1600 [9.99£][2021-01-30]
+FUT 21 – FIFA Points 1600 [9.99£][2020-11-18]
+FUT 21 – FIFA Points 1600 [9.99£][2020-10-22]
+FIFA 21 Standard Edition PS4™ & PS5™ [49.99£][2020-10-15]
+PGA TOUR 2K21 [41.66£][2020-08-26]
+South Park™: The Fractured but Whole™ - SEASON PASS [6.66£][2020-06-22]
+Mafia II: Definitive Edition [20.82£][2020-06-22]
+Madden NFL 20 [13.32£][2020-06-13]
+NBA 2K20 [3.32£][2020-05-17]
+Tennis World Tour [5.41£][2020-05-17]
+Snooker 19 [12.49£][2020-05-11]
+FIFA Points 250 [1.66£][2020-04-18]
+FIFA Points 1050 [6.66£][2020-04-18]
+FIFA Points 1050 [6.66£][2020-04-16]
+STAR WARS Jedi: Fallen Order™ [29.16£][2020-04-07]
+eFootball PES 2020 Standard Edition [13.32£][2020-04-07]
+PLAYERUNKNOWN'S BATTLEGROUNDS [6.66£][2020-03-08]
+FIFA Points 250 [1.66£][2020-02-14]
+FIFA Points 250 [1.66£][2020-02-06]
+FIFA Points 1050 [6.66£][2020-02-06]
+Mafia III [7.49£][2020-01-03]
+FIFA Points 250 [1.66£][2019-12-26]
+FIFA Points 250 [1.66£][2019-12-26]
+FIFA Points 2200 [13.32£][2019-12-26]
+NBA 2K Playgrounds 2 [7.49£][2019-11-28]
+Peggle 2 [8.32£][2019-11-05]
+Call of Duty®: Modern Warfare® [49.99£][2019-11-03]
+FIFA Points 1050 [6.66£][2019-10-30]
+FIFA Points 1050 [6.66£][2019-10-04]
+FIFA Points 250 [1.66£][2019-09-26]
+FIFA Points 1050 [6.66£][2019-09-26]
+EA SPORTS™ FIFA 20 Champions Edition [66.66£][2019-09-24]
+FIFA Points 2,200 [13.32£][2019-07-29]
+FIFA Points 100 [0.66£][2019-06-22]
+FIFA Points 500 [3.32£][2019-06-22]
+Grand Theft Auto: San Andreas [5.16£][2019-06-02]
+The Warriors [5.16£][2019-06-02]
+FIFA Points 1,600 [9.99£][2019-05-27]
+NBA 2K19 20th Anniversary Edition [29.16£][2019-03-27]
+FIFA Points 1,600 [9.99£][2019-03-26]
+FIFA Points 1,050 [6.66£][2019-01-26]
+EA SPORTS™ UFC® 3 [13.32£][2019-01-16]
+Uncharted™: The Nathan Drake Collection [13.32£][2019-01-16]
+FIFA Points 1,600 [9.99£][2019-01-04]
+RUGBY 18 [10.82£][2019-01-01]
+Pure Pool Snooker Bundle [2.32£][2019-01-01]
+==============================================================
+==============================================================
+
+x-x-x ACCOUNT: ravjyots@gmail.com:frenzy11
+---CheckTime: 2021/02/14 04:12 AM
+---ONLINE ID: ravjyot
+---COUNTRY: IN
+---DATEOFBIRTH: 1987-06-11
+---DEACTIVATION: Yes
+---DEVICES: PS4[1] 
+---PLAYSTATION PLUS: PlayStation Plus: 12 Month Membership [01/12/2022]
+---PAYMENT METHODS: [VISA 2022-3] 
+---BALANCE: Rs 0
+---HAS MORE TRANSACTIONS: NO
+-------------------- Transactions --------------------
+Destiny 2: Beyond Light Deluxe Edition [37.7Rs][2020-11-15]
+Destiny 2: Shadowkeep Digital Deluxe Edition [29.65Rs][2019-09-25]
+Anthem™ Standard Edition [28.6Rs][2019-01-13]
+Destiny 2: Forsaken + Annual Pass [33.89Rs][2018-09-04]
+The Witcher 3: Wild Hunt – Game of the Year Edition [16.94Rs][2018-04-02]
+Battlefield™ 1 Premium Pass [10.58Rs][2018-01-09]
+Destiny 2 - Digital Deluxe Edition [61.86Rs][2017-09-05]
+==============================================================
+==============================================================
+
+x-x-x ACCOUNT: ferrismahmudah@gmail.com:ferris007
+---CheckTime: 2021/02/14 01:44 AM
+---ONLINE ID: GhasanMudah
+---COUNTRY: ZA
+---DATEOFBIRTH: 1986-12-31
+---DEACTIVATION: Yes
+---DEVICES: PS4[1] 
+---PLAYSTATION PLUS: PlayStation Plus: 12 Month Membership [07/13/2021]
+---PAYMENT METHODS: [MC 2024-11] 
+---BALANCE: R 0.00
+---HAS MORE TRANSACTIONS: NO
+-------------------- Transactions --------------------
+2,400 Call of Duty®: Modern Warfare® Points [286.09R][2020-12-30]
+EA SPORTS™ FIFA 20 [138.26R][2020-06-16]
+PlayStation®Plus: 30% off 12 Month Membership [455.65R][2020-06-16]
+Need for Speed™ [77.39R][2019-12-26]
+PlayStation®Plus: 12 Month Membership 20% Off [625.22R][2018-11-23]
+==============================================================
+==============================================================
+
+x-x-x ACCOUNT: andres.fong@gmail.com:Andres1998
+---CheckTime: 2021/02/14 01:55 AM
+---ONLINE ID: AEFC98
+---COUNTRY: MX
+---DATEOFBIRTH: 1998-02-25
+---DEACTIVATION: Yes
+---DEVICES: PS4[1] 
+---PLAYSTATION PLUS: NO
+---PAYMENT METHODS: N/A
+---BALANCE: US$0.00
+---HAS MORE TRANSACTIONS: NO
+-------------------- Transactions --------------------
+200 Call of Duty®: Modern Warfare® Points [1.99US$][2020-12-06]
+Call of Duty®: Modern Warfare® - Battle Pass Edition [59.99US$][2020-07-29]
+Grand Theft Auto V: Premium Edition [14.99US$][2020-04-20]
+==============================================================
+==============================================================
+
+x-x-x ACCOUNT: ms.sexc10@gmail.com:jjordan8
+---CheckTime: 2021/02/14 02:08 AM
+---ONLINE ID: cece1doe
+---COUNTRY: US
+---DATEOFBIRTH: 1989-08-06
+---DEACTIVATION: Yes
+---DEVICES: PS4[1] 
+---PLAYSTATION PLUS: PlayStation Plus 12-Month Subscription [04/05/2021]
+---PAYMENT METHODS: [VISA 2023-12] [MC 2023-10] [VISA 2022-12] 
+---BALANCE: $0.00
+---HAS MORE TRANSACTIONS: NO
+-------------------- Transactions --------------------
+Online ID Change [4.99$][2021-01-23]
+RISK [5.99$][2020-05-13]
+MONOPOLY PLUS [14.99$][2020-05-13]
+UNO® Ultimate Edition  [8.99$][2020-05-13]
+Fortnite - 2,800 V-Bucks [24.99$][2020-04-24]
+Starter Pack and Whale Shark Card Bundle [59.99$][2020-04-07]
+Crash Bandicoot™ N. Sane Trilogy [39.99$][2017-10-11]
+==============================================================
+==============================================================
+
+x-x-x ACCOUNT: luisgonz84@gmail.com:forifesta1984
+---CheckTime: 2021/02/14 02:18 AM
+---ONLINE ID: VivoEnUnSaco
+---COUNTRY: ES
+---DATEOFBIRTH: 1984-10-21
+---DEACTIVATION: Yes
+---DEVICES: PS4[1] 
+---PLAYSTATION PLUS: NO
+---PAYMENT METHODS: [VISA 2021-3] [VISA 2020-12] 
+---BALANCE: 0,00 €
+---HAS MORE TRANSACTIONS: NO
+-------------------- Transactions --------------------
+PlayStation Now: 1 Month Subscription [8.26€][2021-02-12]
+MotoGP™19 [9.5€][2020-03-24]
+==============================================================
+==============================================================
+
+x-x-x ACCOUNT: victorrebellon@hotmail.com:peterete8
+---CheckTime: 2021/02/14 02:27 AM
+---ONLINE ID: VictorRebellon
+---COUNTRY: ES
+---DATEOFBIRTH: 1986-07-10
+---DEACTIVATION: Yes
+---DEVICES: PS4[1] 
+---PLAYSTATION PLUS: NO
+---PAYMENT METHODS: [VISA 2024-8] 
+---BALANCE: 0,00 €
+---HAS MORE TRANSACTIONS: NO
+-------------------- Transactions --------------------
+STAR WARS™ Battlefront™ II: Celebration Edition [9.91€][2021-01-19]
+TEKKEN 7 [8.26€][2020-11-03]
+God of War™ [16.52€][2020-11-03]
+eFootball PES 2020 Standard Edition [16.52€][2020-04-11]
+The Crew® 2 - Deluxe Edition [12.39€][2020-04-01]
+Ghost Recon® Breakpoint [16.52€][2020-03-29]
+MX vs. ATV Supercross Encore [4.12€][2020-03-28]
+PRO EVOLUTION SOCCER 2019 STANDARD EDITION [20.65€][2019-01-03]
+==============================================================
+==============================================================
+
+x-x-x ACCOUNT: jesusren22@hotmail.com:jesus2004
+---CheckTime: 2021/02/14 02:47 AM
+---ONLINE ID: Big_Red1022
+---COUNTRY: US
+---DATEOFBIRTH: 1993-10-22
+---DEACTIVATION: Yes
+---DEVICES: PS4[1] 
+---PLAYSTATION PLUS: NO
+---PAYMENT METHODS: N/A
+---BALANCE: $0.00
+---HAS MORE TRANSACTIONS: NO
+-------------------- Transactions --------------------
+15,000 VC (NBA 2K20) [4.99$][2020-06-06]
+15,000 VC (NBA 2K20) [4.99$][2020-06-06]
+Call of Duty®: Modern Warfare® [39.59$][2020-01-02]
+5,000 VC [1.69$][2019-07-31]
+STAR WARS™ Battlefront™ II [7.49$][2019-07-26]
+LittleBigPlanet™ 3 [19.99$][2019-07-19]
+Grand Theft Auto V [14.99$][2019-06-07]
+Minecraft: PlayStation®4 Edition [9.99$][2019-05-19]
+NBA 2K19 [19.79$][2019-03-19]
+Rocket League® [9.99$][2019-01-05]
+Fortnite Battle Royale - Starter Pack [4.99$][2018-05-05]
+Fortnite - 1,000 V-Bucks [9.99$][2018-05-01]
+==============================================================
+==============================================================
+
+x-x-x ACCOUNT: matty71604@gmail.com:bruinsfan1
+---CheckTime: 2021/02/14 03:04 AM
+---ONLINE ID: WickedestCash69
+---COUNTRY: US
+---DATEOFBIRTH: 1997-09-06
+---DEACTIVATION: Yes
+---DEVICES: PS4[1] 
+---PLAYSTATION PLUS: PlayStation Plus 3-Month Subscription [04/01/2021]
+---PAYMENT METHODS: [VISA 2024-10] [VISA 2022-2] [MC 2021-3] 
+---BALANCE: $0.00
+---HAS MORE TRANSACTIONS: NO
+-------------------- Transactions --------------------
+Minecraft Dungeons Hero Edition [29.99$][2021-01-17]
+DOOM Eternal Standard Edition [19.79$][2020-12-26]
+Ghost of Tsushima [40.19$][2020-12-26]
+Red Dead Redemption 2:  Ultimate Edition [34.99$][2020-12-19]
+Call of Duty®: Black Ops Cold War - Standard Edition [59.99$][2020-11-13]
+Assassin's Creed Valhalla [59.99$][2020-11-09]
+Fortnite: Save the World - Deluxe Founder's Pack [29.99$][2018-02-25]
+==============================================================
+==============================================================
+
+x-x-x ACCOUNT: artemz1996@ukr.net:ogisas96
+---CheckTime: 2021/02/14 03:09 AM
+---ONLINE ID: ARHE-de-SKORPION
+---COUNTRY: UA
+---DATEOFBIRTH: 1996-12-22
+---DEACTIVATION: Yes
+---DEVICES: PS4[1] 
+---PLAYSTATION PLUS: NO
+---PAYMENT METHODS: [MC 2023-3] [VISA 2022-5] [MC 2024-11] 
+---BALANCE: 0.00 UAH
+---HAS MORE TRANSACTIONS: NO
+-------------------- Transactions --------------------
+Mafia III Deluxe Edition [499.0UAH][2020-04-07]
+Red Dead Redemption 2: Special Edition [1349.0UAH][2019-06-28]
+Mortal Kombat XL [499.0UAH][2019-06-27]
+The Forest [359.0UAH][2019-06-21]
+inFAMOUS Second Son™ [499.0UAH][2019-06-15]
+Friday the 13th: The Game [619.0UAH][2019-06-09]
+The Witcher 3: Wild Hunt – Game of the Year Edition [489.0UAH][2019-04-25]
+Grand Theft Auto V: Premium Edition [499.0UAH][2019-04-20]
+==============================================================
+==============================================================
+
+x-x-x ACCOUNT: pwrhodes20@yahoo.com:Jayhawks20
+---CheckTime: 2021/02/14 03:20 AM
+---ONLINE ID: pwrhodes20
+---COUNTRY: US
+---DATEOFBIRTH: 1990-09-27
+---DEACTIVATION: Yes
+---DEVICES: PS4[1] 
+---PLAYSTATION PLUS: NO
+---PAYMENT METHODS: [MC 2022-4] 
+---BALANCE: $0.00
+---HAS MORE TRANSACTIONS: NO
+-------------------- Transactions --------------------
+Call of Duty®: Black Ops III - Zombies Chronicles Edition [59.99$][2020-12-28]
+Call of Duty®: Black Ops III - Der Eisendrache Zombies Map [7.99$][2020-12-28]
+Call of Duty® Black Ops III - Gorod Krovi Zombies Map [7.99$][2020-12-28]
+Call of Duty®: Black Ops Cold War - Standard Edition [59.99$][2020-12-12]
+The Elder Scrolls V: Skyrim Special Edition [15.99$][2020-11-29]
+ARK: Survival Evolved [14.99$][2020-04-25]
+Far Cry® 4   [19.99$][2020-03-28]
+Borderlands 3: Moxxi's Heist of the Handsome Jackpot PS4™ &  PS5™ [14.99$][2020-01-24]
+Far Cry New Dawn [39.99$][2019-11-08]
+Borderlands: Ultimate Edition [29.99$][2017-09-13]
+==============================================================
+==============================================================
+
+x-x-x ACCOUNT: diegobrenes04@outlook.com:Yeyo040103
+---CheckTime: 2021/02/14 03:24 AM
+---ONLINE ID: Killervader04
+---COUNTRY: CR
+---DATEOFBIRTH: 1990-01-04
+---DEACTIVATION: Yes
+---DEVICES: PS4[1] 
+---PLAYSTATION PLUS: PS Plus: Suscripción de 3 Meses Month [02/14/2021]
+---PAYMENT METHODS: [VISA 2024-11] [VISA 2021-7] 
+---BALANCE: US$0.00
+---HAS MORE TRANSACTIONS: NO
+-------------------- Transactions --------------------
+Persona® 5 Strikers Digital Deluxe Edition [69.99US$][2021-02-13]
+Assassin’s Creed® Syndicate [29.99US$][2021-02-02]
+Marvel's Spider-Man: The City That Never Sleeps [24.99US$][2021-01-07]
+Assassin’s Creed® Unity [8.99US$][2020-12-21]
+P3D/P5D: 'Break Out Of... (OP ver.)' [0.99US$][2020-12-20]
+P3D/P5D: Goro Akechi in 'Will Power' [4.99US$][2020-10-07]
+Persona Dancing: Endless Night Collection [21.99US$][2020-10-07]
+WORLD OF FINAL FANTASY [12.49US$][2020-10-05]
+Genshin Impact-Adventurer's Starter Bundle [9.99US$][2020-09-23]
+FINAL FANTASY VII REMAKE [39.59US$][2020-08-14]
+SWORD ART ONLINE Alicization Lycoris Soundtrack Bundle [1.99US$][2020-07-11]
+EA Family Bundle [9.99US$][2020-06-17]
+Mortal Kombat X [5.99US$][2020-06-17]
+Marvel vs. Capcom: Infinite - Standard Edition [9.99US$][2020-06-09]
+ Persona®5 Royal [59.99US$][2020-04-24]
+KINGDOM HEARTS III Re Mind [29.99US$][2020-01-26]
+KINGDOM HEARTS III - 'Midnight Blue' Keyblade [2.99US$][2019-12-31]
+Persona 5 [19.99US$][2019-12-31]
+==============================================================
+==============================================================
+
+x-x-x ACCOUNT: boki-23@hotmail.com:Boki2309
+---CheckTime: 2021/02/14 03:35 AM
+---ONLINE ID: Sekulo23
+---COUNTRY: DK
+---DATEOFBIRTH: 1987-09-23
+---DEACTIVATION: Yes
+---DEVICES: PS4[1] 
+---PLAYSTATION PLUS: PlayStation Plus 1 Month Membership [03/11/2021]
+---PAYMENT METHODS: [VISA 2022-12] 
+---BALANCE: Kr 0,00
+---HAS MORE TRANSACTIONS: NO
+-------------------- Transactions --------------------
+FIFA 21 Champions Edition PS4™ & PS5™ [156.58Kr][2021-02-11]
+Assassin's Creed® Liberation HD [24.0Kr][2018-03-26]
+Assassin's Creed® Freedom Cry [24.0Kr][2018-03-26]
+Dante's Inferno™ Super Bundle [29.6Kr][2018-03-26]
+Alice: Madness Returns™ [17.6Kr][2018-03-26]
+Tomb Raider Digital Edition [8.8Kr][2018-03-26]
+Dogfight 1942 Bundle [12.0Kr][2018-03-26]
+==============================================================
+==============================================================
+
+x-x-x ACCOUNT: serge-lysenk@mail.ru:47t28157bt
+---CheckTime: 2021/02/14 03:52 AM
+---ONLINE ID: Serepano4ka
+---COUNTRY: RU
+---DATEOFBIRTH: 1985-08-17
+---DEACTIVATION: Yes
+---DEVICES: PS4[1] 
+---PLAYSTATION PLUS: PlayStation Plus:   12  Month [04/01/2022]
+---PAYMENT METHODS: [VISA 2022-10] 
+---BALANCE: RUB 0
+---HAS MORE TRANSACTIONS: NO
+-------------------- Transactions --------------------
+EA Play 1 Month Discount Offer [74.17RUB][2021-02-07]
+Minecraft [1124.17RUB][2021-02-02]
+Overcooked [228.33RUB][2021-01-01]
+Overcooked! 2 [670.0RUB][2021-01-01]
+==============================================================
+by Ahmedo Miziry Telegram: @AhmedoPlus
+
+
